0d5b05fe63180f05fed4ecf2177950217c255500
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2026-05-22 19:03:49 -0400

    Update map-coordinate-spaces.js
    fixed again

--- map/utils/map-coordinate-spaces.js
+++ map/utils/map-coordinate-spaces.js
@@ -28,12 +28,12 @@ export function pixels_to_world_units(x, y, svg, viewport_relative) {
    }
    x = x / x_scale + real_rect.x;
    y = y / y_scale + real_rect.y;
-   y = -y;
+   //y = -y;
    return [x, y];
 }
 export function world_units_to_pixels(x, y, svg, viewport_relative) {
    let [disp_rect, real_rect, x_scale, y_scale] = _deconstruct(svg);
-   y = -y;
+   //y = -y;
    x = (x - real_rect.x) * x_scale;
    y = (y - real_rect.y) * y_scale;
    if (viewport_relative) {
@@ -58,6 +58,7 @@ export function world_units_to_grid_units(x, y) {
    grid_x /= CELL_SIZE_WU;
    grid_y /= CELL_SIZE_WU;
    grid_x = Math.floor(grid_x);
-   grid_y = Math.ceil(grid_y);
+   //grid_y = Math.ceil(grid_y);
+   grid_y = Math.floor(grid_y);
    return [grid_x, grid_y];
 }
